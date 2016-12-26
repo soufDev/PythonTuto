cbc43006122d21dece51c74a2c174c1709d87015
soufDev <ait.akkache.soufiane@gmail.com>
2016-12-26 12:45:10 +0100

    Password Management

--- StandardLibraries/PasswordManagement.py
+++ StandardLibraries/PasswordManagement.py
@@ -1,2 +1,23 @@
 # -*-coding:Latin-1 -*
 
+import hashlib
+from getpass import getpass
+
+
+password = b"azerty"
+encrypted_password = hashlib.sha1(password).hexdigest()
+
+lock = True
+while lock:
+    enter = getpass("Enter password : ")
+    enter = enter.encode()
+
+    encrypted_enter = hashlib.sha1(enter).hexdigest()
+    if encrypted_enter == encrypted_password:
+        lock = False
+    else:
+        print("Incorect Password")
+
+print("Password Accepted...")
+
+
